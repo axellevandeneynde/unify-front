77574afef5181f46181bfd07fcde7b18d2d505ad
Axelle Vanden Eynde <axelle.vandeneynde@studiohyperdrive.be>
2021-04-29 11:15:08 +0200

    feat: routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import HomePage from './pages/home/home.page';
+
 function App() {
-  return (<h1>Unify</h1>);
+  return (<Router>
+    {/* add navigation here */}
+    <Switch>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  </Router>);
 }
 
 export default App;
---
+++ src/pages/home/home.page.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return (<p>home is working</p>);
+}
+
+export default HomePage;
